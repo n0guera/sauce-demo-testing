964cd16b41546a4e1e2e54eed36430e25c3c7493
Santiago Noguera <santiiago.noguera@gmail.com>
2026-06-02 14:31:19 -0300

    Add Cypress commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,24 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (user, pass) => {
+  cy.visit("https://www.saucedemo.com/");
+  cy.get('[data-test="username"]').type(user);
+  cy.get('[data-test="password"]').type(pass);
+  cy.get('[data-test="login-button"]').click();
+  cy.url().should("include", "/inventory.html");
+});
+
+Cypress.Commands.add("agregarAlCarrito", (producto) => {
+  cy.get(`[data-test="add-to-cart-${producto}"]`).click();
+});
+
+Cypress.Commands.add("irAlCarrito", () => {
+  cy.get(".shopping_cart_link").click();
+  cy.url().should("include", "/cart.html");
+});
+
+Cypress.Commands.add("confirmarPedido", () => {
+  cy.get('[data-test="finish"]').click();
+  cy.get(".complete-header").should("have.text", "Thank you for your order!");
+});
